34a865070b06a7975bbeeaf91232dd7d052e860a
Alejandro Giraldo S <giraldosernaalejandro16@gmail.com>
2024-06-21 13:10:19 -0500

    User detail

---
+++ src/main/java/com/riwi/workShop/api/dto/response/UserDetailResponse.java
@@ -0,0 +1,22 @@
+package com.riwi.workShop.api.dto.response;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class UserDetailResponse {
+    private String id;
+    private String username;
+    private String email;
+    private String fullName;
+    private String role;
+    private List<LoanResponse> loans;
+    private List<ReservationResponse> reservations;
+}
